18e3d427ccece279afefc9696ab81f8f56c9bbc1
Tauno Tamm. <tauno171@gmail.com>
2024-09-15 13:14:01 +0300

    add week2 code

---
+++ UserAgeValidation1/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter your age:");
+
+string userAge = Console.ReadLine();
+
+int userAgeNum = 0;
+
+//boolean - true/false
+
+bool isAgeNumber = Int32.TryParse(userAge, out userAgeNum);
+
+Console.WriteLine($"Parse result {isAgeNumber}. User is {userAgeNum} years old");
+
+if(isAgeNumber == true)
+{
+    if (userAgeNum >= 13)
+    {
+        Console.WriteLine("Welcome to Instagram!");
+    }
+    else
+    {
+        Console.WriteLine("You are too young for Instagram");
+    }
+}
+else
+{
+    Console.WriteLine("Could not read your age");
+}
---
+++ Week2/Program.cs
@@ -0,0 +1,4 @@
+﻿// Int ja Char
+//If-statemetns
+Console.WriteLine("Hello World");
+
